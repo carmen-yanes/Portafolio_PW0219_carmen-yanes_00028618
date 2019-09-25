84b90cc767506d1b84af2d571d6209a1eb9a5755
carmen-yanes <00028618@uca.edu.sv>
2019-09-25 09:31:17 -0600

    Subiendo Labo4

---
+++ Laboratorios/Laboratorio4/Practica/index.js
@@ -0,0 +1,10 @@
+
+var a = 'Hola mundo'
+
+console.log(a)
+
+miFuncion();
+function miFUncion(){
+    console.log("llamada desde la funcion")
+}
+
---
+++ Laboratorios/Laboratorio4/ejercicio1.js
@@ -0,0 +1,7 @@
+//Realice una función que reciba un arreglo como parámetro y liste los tipos de datos de sus elementos y cuantos de cada tipo. 
+
+
+
+function array(){
+    
+}
